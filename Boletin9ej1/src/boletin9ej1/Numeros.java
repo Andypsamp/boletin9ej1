4fff051499af2631dc2c6eca67d1bedcc3328bab
apenasampedro <apenasampedro@eniac26.danielcastelao.org>
2014-11-17 09:20:52 +0100

    pongo metodopublic

---
+++ Boletin9ej1/src/boletin9ej1/Numeros.java
@@ -0,0 +1,40 @@
+
+package boletin9ej1;
+
+import javax.swing.JOptionPane;
+
+public class Numeros {
+  private int numero;
+    //constructor por defecto
+    public Numeros(){
+        
+    }
+    //constructor con parametro
+    public Numeros(int num1){
+        this.numero=num1;
+    }
+    //metodo pedirDato
+    public int pedirDato() {
+        String respuesta = JOptionPane.showInputDialog("INTRODUCE NUMERO"); 
+        int valor = Integer.parseInt(respuesta);
+        return valor;
+    }
+    public void sacarNumeros(int num1){
+         Numeros obj=new Numeros();
+         //estructura repetitiva
+        for(num1=1;num1<=10;num1++){
+          
+             int valor =obj.pedirDato();
+           
+            if (valor>0){
+                JOptionPane.showMessageDialog(null, "El Numero es positivo");
+            }
+            if (valor<0){
+                JOptionPane.showMessageDialog(null, "El Numero es negativo");
+            }
+            if (valor==0){
+                JOptionPane.showMessageDialog(null, "Es cero");
+            }
+        }
+    }   
+}
